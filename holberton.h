7fa109012d2cbe24725ba6c1d810934bacbf1ddf
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-04-05 13:19:46 +0000

    files

---
+++ holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*protoypes*/
+
+#endif
